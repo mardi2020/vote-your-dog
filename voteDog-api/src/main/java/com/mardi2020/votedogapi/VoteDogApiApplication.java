fa80600accfa98a47cfd96bea79d227d8e47317b
mardi2020 <mardi@khu.ac.kr>
2023-05-23 16:58:59 +0900

    chore: api 서버 모듈 추가

---
+++ voteDog-api/src/main/java/com/mardi2020/votedogapi/VoteDogApiApplication.java
@@ -0,0 +1,13 @@
+package com.mardi2020.votedogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteDogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteDogApiApplication.class, args);
+    }
+
+}
---
+++ voteDog-api/src/test/java/com/mardi2020/votedogapi/VoteDogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mardi2020.votedogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteDogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
