0a86dc0d9bf49ca65ae21ae2b8893ed3ee356f03
mardi2020 <mardi@khu.ac.kr>
2023-05-25 18:40:50 +0900

    chore: sql 더미데이터 파일 추가

---
+++ voteDog-voteUpdate/src/main/resources/script/dogs.sql
@@ -0,0 +1,7 @@
+use voteDog;
+insert into dogs (id, count, description, image, name)
+values  (1, 50, '내 갱얼쥐', 'https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/holapet/20210525081724428qquq.jpg', '코코'),
+        (2, 30, '기여운 강아지', 'https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/holapet/20210525081725668haup.png', '초코'),
+        (3, 30, '백설기아니고 강아지입니다', 'https://img3.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/holapet/20210525081726892iyzj.jpg', '시로'),
+        (4, 100, '장인 장모 치와와', 'https://img4.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/holapet/20210525081729350fczm.jpg', '설탕이'),
+        (5, 70, '솜사탕아니고 강아지', 'https://images.mypetlife.co.kr/content/uploads/2022/06/28123435/KakaoTalk_20220602_090443576-1-1-edited-scaled.jpg', '봄');
