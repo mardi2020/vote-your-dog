a37d3f16bda9e409adbbec2655e100b37b4ef913
mardi2020 <mardi@khu.ac.kr>
2023-06-01 19:40:09 +0900

    refactor: api 서버 카프카 수정

--- voteDog-api/src/main/java/com/mardi2020/votedogapi/Dog/Service/DogKafkaProducer.java
+++ voteDog-api/src/main/java/com/mardi2020/votedogapi/Dog/Service/DogKafkaProducer.java
@@ -1,7 +1,5 @@
 package com.mardi2020.votedogapi.Dog.Service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mardi2020.votedogcommon.Dog.Enum.VoteStatus;
 import com.mardi2020.votedogcommon.Dog.Message.DogVoteUpdate;
 import com.mardi2020.votedogcommon.Dog.Util.KafkaTopic;
@@ -15,21 +13,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class DogKafkaProducer {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
-
-    private final ObjectMapper mapper;
-
-    private void send(final String topic, final DogVoteUpdate data) {
-        String message;
-        try {
-            message = mapper.writeValueAsString(data);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-
-        kafkaTemplate.send(topic, message);
-        log.info("Kafka produces message: " + message);
-    }
+    private final KafkaTemplate<String, DogVoteUpdate> kafkaTemplate;
 
     public void sendMessage(final Long newDogId,
                             final String beforeDogId,
@@ -40,6 +24,7 @@ public class DogKafkaProducer {
                 .status(status)
                 .build();
 
-        send(KafkaTopic.VOTE_BEFORE, dogVoteUpdate);
+        kafkaTemplate.send(KafkaTopic.VOTE_BEFORE, dogVoteUpdate);
+        log.info("Kafka produces message: " + dogVoteUpdate);
     }
 }
