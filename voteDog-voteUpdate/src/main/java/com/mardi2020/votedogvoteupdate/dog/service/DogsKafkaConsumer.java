aaddc1de43b594321f049e657b2b66a232b3bf14
mardi2020 <mardi@khu.ac.kr>
2023-06-13 20:07:49 +0900

    refactor: modifyDog 메서드 리팩토링

--- voteDog-voteUpdate/src/main/java/com/mardi2020/votedogvoteupdate/dog/service/DogsKafkaConsumer.java
+++ voteDog-voteUpdate/src/main/java/com/mardi2020/votedogvoteupdate/dog/service/DogsKafkaConsumer.java
@@ -8,6 +8,9 @@ import com.mardi2020.votedogcommon.dog.util.KafkaTopic;
 import com.mardi2020.votedogvoteupdate.dog.Dog;
 import com.mardi2020.votedogvoteupdate.dog.repository.DogRepository;
 import jakarta.transaction.Transactional;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -16,7 +19,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 @Slf4j
 public class DogsKafkaConsumer {
 
@@ -26,41 +28,67 @@ public class DogsKafkaConsumer {
 
     @KafkaListener(topics = KafkaTopic.VOTE_BEFORE,
             containerFactory = "dogVoteUpdateConcurrentKafkaListenerContainerFactory")
+    @Transactional
     public void modifyDog(final ConsumerRecord<String, DogVoteUpdate> record) {
         final DogVoteUpdate data = record.value();
-        final VoteStatus status = data.getStatus();
-        DogParam dogParam = new DogParam(status, data.getNewDogId(), data.getBeforeDogId());
-
-        if (!status.equals(VoteStatus.ANOTHER)) {
-            final Dog newDog = handleStatus(data.getNewDogId(), status);
-            dogParam.setNewDogCount(newDog.getCount());
-        } else {
-            final Dog beforeDog = decreaseVoteCount(data.getBeforeDogId());
-            final Dog newDog = increaseVoteCount(data.getNewDogId());
-            dogParam.setBeforeDogCount(beforeDog.getCount());
-            dogParam.setNewDogCount(newDog.getCount());
-        }
+        final WrapperDogs dogs = updateDogCount(data);
+        DogParam dogParam = createMessageByStatus(data, dogs);
         dogKafkaProducer.send(KafkaTopic.VOTE_AFTER, dogParam);
     }
 
-    private Dog increaseVoteCount(Long id) {
+    public WrapperDogs updateDogCount(DogVoteUpdate updateData) {
+        if (!updateData.getStatus().equals(VoteStatus.ANOTHER)) {
+            return new WrapperDogs(handleStatus(updateData));
+        }
+        final Dog beforeDog = decreaseVoteCount(updateData.getBeforeDogId());
+        final Dog newDog = increaseVoteCount(updateData.getNewDogId());
+        return new WrapperDogs(newDog, beforeDog);
+    }
+
+    public DogParam createMessageByStatus(final DogVoteUpdate updateData, final WrapperDogs dogs) {
+        DogParam dogParam = new DogParam(updateData);
+        dogParam.setNewDogCount(dogs.getNewDog().getCount());
+        if (!updateData.getStatus().equals(VoteStatus.ANOTHER)) {
+            return dogParam;
+        }
+        dogParam.setBeforeDogCount(dogs.getBeforeDog().getCount());
+        return dogParam;
+    }
+
+    public Dog increaseVoteCount(Long id) {
         final Dog dog = dogRepository.findById(id)
                 .orElseThrow(DogNotFoundException::new);
         dog.increaseCount();
         return dogRepository.save(dog);
     }
 
-    private Dog decreaseVoteCount(Long id) {
+    public Dog decreaseVoteCount(Long id) {
         final Dog dog = dogRepository.findById(id)
                 .orElseThrow(DogNotFoundException::new);
         dog.decreaseCount();
         return dogRepository.save(dog);
     }
 
-    private Dog handleStatus(Long id, VoteStatus status) {
-        if (status.equals(VoteStatus.INIT)) {
-            return increaseVoteCount(id);
+    public Dog handleStatus(DogVoteUpdate updateData) {
+        if (updateData.getStatus().equals(VoteStatus.INIT)) {
+            return increaseVoteCount(updateData.getNewDogId());
         }
-        return decreaseVoteCount(id);
+        return decreaseVoteCount(updateData.getNewDogId());
+    }
+
+    @Data
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class WrapperDogs {
+        private Dog newDog;
+        private Dog beforeDog;
+
+        public WrapperDogs(Dog newDog) {
+            this.newDog = newDog;
+        }
+
+        public WrapperDogs(Dog newDog, Dog beforeDog) {
+            this.newDog = newDog;
+            this.beforeDog = beforeDog;
+        }
     }
 }
